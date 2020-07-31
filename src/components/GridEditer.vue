11d6bfe6a7f570f421a5b5b2bab4d6bd55a6cb5d
zhijinwang <zhijinwang@sfmail.sf-express.com>
2020-07-31 21:09:32 +0800

    配置修改

---
+++ src/components/GridEditer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref="map">
+      <h1>我是标题</h1>
+      <div>{{message}}</div>
+  <div>{{propData}}</div>
+  </div>
+</template>
+<script>
+export default{
+    name:'grid-editer',
+    data(){
+        return {
+            message:'123456789'
+        }
+    },
+    mounted(){
+        
+        console.log(8888);
+        console.log(this.$refs.map);
+    },
+props:{
+    propData:{
+        type:String,
+        default:'@室内地图'
+    }
+},
+}
+</script>
+<style>
+
+</style>
